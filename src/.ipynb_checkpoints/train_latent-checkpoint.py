ffc0dbee9cd37d52edfbbd718bdb81468d10c5b5
amirabezine@gmail.com <amirabezine@gmail.com>
2024-06-25 07:36:39 +0000

    working version, saving latent and optimizing

--- src/.ipynb_checkpoints/train_latent-checkpoint.py
+++ src/.ipynb_checkpoints/train_latent-checkpoint.py
@@ -36,36 +36,43 @@ def save_timing_data(filepath, timing_data):
     # Check if file exists to write headers accordingly
     file_exists = os.path.isfile(filepath)
 
-    with open(filepath, 'a', newline='') as file:
-        writer = csv.writer(file)
-        # Write headers if file does not exist
-        if not file_exists:
-            writer.writerow(['Epoch', 'Total Training Time', 'Total Validation Time', 'Weight Optimization Time', 'Latent Optimization Time'])
-        
-        # Write data
-        for data in timing_data:
-            writer.writerow(data)
+    try:
+        with open(filepath, 'a', newline='') as file:
+            writer = csv.writer(file)
+            # Write headers if file does not exist
+            if not file_exists:
+                writer.writerow(['Epoch', 'Total Training Time', 'Total Validation Time', 'Weight Optimization Time', 'Latent Optimization Time'])
+            
+            # Write data
+            for data in timing_data:
+                writer.writerow(data)
+    
+    except OSError as e:
+        print(f"Failed to open file {filepath}: {e}")
 
-        
+
 def load_latent_vectors(hdf5_path, loaders, latent_dim, device):
     latent_vectors = []
     dict_latent_codes = {}  # Dictionary to store the mapping from unique_index to tensor index
 
-    with h5py.File(hdf5_path, 'r') as hdf5_file:
-        idx = 0
-        for loader in loaders:  # Loop over both train_loader and val_loader
-            for batch in loader:
-                unique_ids = batch['unique_id'] 
-                for unique_id in unique_ids:
-                    dict_latent_codes[unique_id] = idx
-                    if unique_id in hdf5_file:
-                        data = torch.tensor(hdf5_file[unique_id]['latent_code'][()], dtype=torch.float32, device=device)
-                        latent_vectors.append(data)
-                    else:
-                        # Append zeros if no data found
-                        latent_vectors.append(torch.zeros(latent_dim, device=device))
-                    idx += 1
 
+    try:
+        with h5py.File(hdf5_path, 'r') as hdf5_file:
+            idx = 0
+            for loader in loaders:  # Loop over both train_loader and val_loader
+                for batch in loader:
+                    unique_ids = batch['unique_id'] 
+                    for unique_id in unique_ids:
+                        dict_latent_codes[unique_id] = idx
+                        if unique_id in hdf5_file:
+                            data = torch.tensor(hdf5_file[unique_id]['latent_code'][()], dtype=torch.float32, device=device)
+                            latent_vectors.append(data)
+                        else:
+                            # Append zeros if no data found
+                            latent_vectors.append(torch.zeros(latent_dim, device=device))
+                        idx += 1
+    except OSError as e:
+        print(f"Failed to open file {hdf5_path}: {e}")
     # Stack all vectors to create the final tensor
     latent_codes = torch.stack(latent_vectors, dim=0).requires_grad_(True)
     return latent_codes, dict_latent_codes
@@ -74,27 +81,35 @@ def load_latent_vectors(hdf5_path, loaders, latent_dim, device):
 
 
 def save_latent_vectors_to_hdf5(hdf5_path, dict_latent_codes, latent_vectors, epoch):
-    with h5py.File(hdf5_path, 'a') as hdf5_file:
-        for unique_id, index in dict_latent_codes.items():
-            decoded_id = unique_id.decode('utf-8') if isinstance(unique_id, bytes) else unique_id
-            versioned_key = f"{decoded_id}/optimized_latent_code/epoch_{epoch}"
-            # versioned_key = f"{unique_id}/latent_code_epoch_{epoch}"
-            # print(versioned_key)
-            if versioned_key in hdf5_file:
-                del hdf5_file[versioned_key]  # Remove the old dataset if it exists
-            hdf5_file[versioned_key] = latent_vectors[index].cpu().detach().numpy()  # Create a new dataset
-            # print("saved  ", versioned_key)
+
+    try:
+        with h5py.File(hdf5_path, 'a') as hdf5_file:
+            for unique_id, index in dict_latent_codes.items():
+                decoded_id = unique_id.decode('utf-8') if isinstance(unique_id, bytes) else unique_id
+                versioned_key = f"{decoded_id}/optimized_latent_code/epoch_{epoch}"
+                # versioned_key = f"{unique_id}/latent_code_epoch_{epoch}"
+                # print(versioned_key)
+                if versioned_key in hdf5_file:
+                    del hdf5_file[versioned_key]  # Remove the old dataset if it exists
+                hdf5_file[versioned_key] = latent_vectors[index].cpu().detach().numpy()  # Create a new dataset
+                # print("saved  ", versioned_key)
+    except OSError as e:
+        print(f"Failed to open file {hdf5_path}: {e}")
 
 
 def save_last_latent_vectors_to_hdf5(hdf5_path, dict_latent_codes, latent_vectors):
-    with h5py.File(hdf5_path, 'a') as hdf5_file:
-        for unique_id, index in dict_latent_codes.items():
-            decoded_id = unique_id.decode('utf-8') if isinstance(unique_id, bytes) else unique_id
-            
-            versioned_key = f"{decoded_id}/optimized_latent_code/latest"
-            if versioned_key in hdf5_file:
-                del hdf5_file[versioned_key]  # Remove the old dataset if it exists
-            hdf5_file[versioned_key] = latent_vectors[index].cpu().detach().numpy()  # Create a new dataset
+
+    try:
+        with h5py.File(hdf5_path, 'a') as hdf5_file:
+            for unique_id, index in dict_latent_codes.items():
+                decoded_id = unique_id.decode('utf-8') if isinstance(unique_id, bytes) else unique_id
+                
+                versioned_key = f"{decoded_id}/optimized_latent_code/latest"
+                if versioned_key in hdf5_file:
+                    del hdf5_file[versioned_key]  # Remove the old dataset if it exists
+                hdf5_file[versioned_key] = latent_vectors[index].cpu().detach().numpy()  # Create a new dataset
+    except OSError as e:
+        print(f"Failed to open file {hdf5_path}: {e}")
 
 
 
@@ -184,14 +199,14 @@ def load_checkpoints(generator, latent_codes, optimizer_g, optimizer_l, checkpoi
 
 
 
-def train_and_validate(generator, latent_codes, dict_latent_codes, optimizer_g, optimizer_l, scheduler_g, scheduler_l, train_loader, val_loader, start_epoch, num_epochs, checkpoints_path, latent_path, device, best_val_loss, data_path):
-     loss_history = {
+def train_and_validate(generator, latent_codes, dict_latent_codes, optimizer_g, optimizer_l, scheduler_g, scheduler_l, train_loader, val_loader, start_epoch, num_epochs, checkpoints_path, latent_path, device, best_val_loss, data_path, plots_path):
+    loss_history = {
         'train': [],
         'val': [],
         'before':[]
     }
-
-    timing_data = []  # Collect timing data for all epochs
+    
+    timing_data = []
 
     for epoch in range(start_epoch, num_epochs):
 
@@ -243,64 +258,114 @@ def train_and_validate(generator, latent_codes, dict_latent_codes, optimizer_g, 
             optimizer_l.step()
             for param in generator.parameters():
                 param.requires_grad = True
-            poch_latent_opt_time += time.time() - optimizer_l_start_time
+            epoch_latent_opt_time += time.time() - optimizer_l_start_time
 
             epoch_losses.append(loss_l.item())
             
 
-            batch_time = time.time() - batch_start_time
+            # batch_time = time.time() - batch_start_time
             
-            train_bar.set_postfix({"Batch Total Time": batch_time, "Batch Loss": loss_l.item(), "Optimizer G Time": optimizer_g_time, "Optimizer L Time": optimizer_l_time})
-            average_train_loss_before = np.mean(epoch_losses_before)
-            average_train_loss = np.mean(epoch_losses)
-            loss_history['train'].append(average_train_loss)
-            loss_history['before'].append(average_train_loss_before)
-            print(f'Epoch {epoch+1} Average Train Loss before latent: {average_train_loss_before:.4f}')
-            print(f'Epoch {epoch+1} Average Train Loss after latent: {average_train_loss:.4f}')
+            # train_bar.set_postfix({"Batch Total Time": batch_time, "Batch Loss": loss_l.item(), "Optimizer G Time": epoch_weight_opt_time, "Optimizer L Time": epoch_latent_opt_time})
+        average_train_loss_before = np.mean(epoch_losses_before)
+        average_train_loss = np.mean(epoch_losses)
+        loss_history['train'].append(average_train_loss)
+        loss_history['before'].append(average_train_loss_before)
+            
+        print(f'Epoch {epoch+1} Average Train Loss before latent: {average_train_loss_before:.4f}')
+        print(f'Epoch {epoch+1} Average Train Loss after latent: {average_train_loss:.4f}')
 
         epoch_train_time = time.time() - epoch_train_time_start
 
+
+
+        
         # Validation phase
         validation_start_time = time.time()
         generator.eval()
         val_losses = []
         val_bar = tqdm(val_loader, desc=f"Validation Epoch {epoch + 1}/{num_epochs}")
-        with torch.no_grad():
-            for batch in val_bar:
-                unique_ids = batch['unique_id']
-                flux = batch['flux'].to(device)
-                mask = batch['flux_mask'].to(device)
-                sigma = batch['sigma'].to(device)
-                sigma_safe = sigma**2 + 1e-5
-                indices = [dict_latent_codes[uid] for uid in unique_ids]
-                generated = generator(latent_codes[indices])
-                val_loss = weighted_mse_loss(generated, flux, mask/sigma_safe)
-                val_losses.append(val_loss.item())
-                val_bar.set_postfix({"Batch Val Loss": val_loss.item()})
-                average_val_loss = np.mean(val_losses)
-                loss_history['val'].append(average_val_loss)
-                print(f'Epoch {epoch+1} Average Validation Loss: {average_val_loss:.4f}')
+
+        
+        for batch in val_bar:
+            unique_ids = batch['unique_id']
+            flux = batch['flux'].to(device)
+            mask = batch['flux_mask'].to(device)
+            sigma = batch['sigma'].to(device)
+            sigma_safe = sigma**2 + 1e-5
+            indices = [dict_latent_codes[uid] for uid in unique_ids]
+
+
+            # Optimize latent codes
+         
+            for param in generator.parameters():
+                param.requires_grad = False
+            latent_codes.requires_grad_(True)
+            optimizer_l.zero_grad()
+            generated = generator(latent_codes[indices])
+            val_loss = weighted_mse_loss(generated, flux, mask/sigma_safe)
+            val_loss.backward()
+            optimizer_l.step()
+            for param in generator.parameters():
+                param.requires_grad = True
+
+            val_losses.append(val_loss.item())
+        
+            # # Ensure generator is in evaluation mode and doesn't track gradients
+            # for param in generator.parameters():
+            #     param.requires_grad = False
+        
+            # # Optimize latent codes
+            # latent_codes.requires_grad_(True)
+            # optimizer_l.zero_grad()
+            # generated = generator(latent_codes[indices])
+            # val_latent_loss = weighted_mse_loss(generated, flux, mask/sigma_safe)
+            # val_latent_loss.backward()
+            # optimizer_l.step()
+            # latent_codes.requires_grad_(False)  # Turn off gradients for latent codes after update
+        
+            # val_losses.append(val_latent_loss.item())
+            # val_bar.set_postfix({"Val Loss": val_latent_loss.item()})
+            # average_val_loss = np.mean(val_losses)
+            # loss_history['val'].append(average_val_loss)
+            # print(f'Epoch {epoch+1} Average Validation Loss: {average_val_loss:.4f}')
 
         average_val_loss = np.mean(val_losses)
-        epoch_val_time = time.time() - validation_start_time        
+        epoch_val_time = time.time() - validation_start_time 
+        # print(f'Epoch {epoch+1} Average Validation Loss: {average_val_loss:.4f}')
+
         # print(f'Epoch {epoch+1} Average Train Loss: {np.mean(epoch_losses):.4f}, Average Validation Loss: {average_val_loss:.4f}')
-        print(f'Epoch {epoch+1} Total Training Time: {total_train_time:.2f} seconds, Total Validation Time: {total_validation_time:.2f} seconds')
+        print(f'Epoch {epoch+1} Average Validation Loss: {average_val_loss:.4f}')
+        loss_history['val'].append(average_val_loss)
 
         scheduler_g.step()
         scheduler_l.step()
 
-        if (epoch + 1) % 10 == 0 or epoch + 1 == num_epochs:
+        if (epoch + 1) % 5 == 0 or epoch + 1 == num_epochs:
             save_latent_vectors_to_hdf5(data_path, dict_latent_codes, latent_codes, epoch + 1)
             print(f"Latent vectors saved for epoch {epoch + 1}")
         
         timing_data.append((epoch + 1, epoch_train_time, epoch_val_time, epoch_weight_opt_time, epoch_latent_opt_time))
 
-
+        # Save checkpoints
+        checkpoint_state = {
+            'epoch': epoch + 1,
+            'state_dict': generator.state_dict(),
+            'optimizer_g_state': optimizer_g.state_dict(),
+            'optimizer_l_state': optimizer_l.state_dict(),
+            'latent_codes': latent_codes.detach().cpu(),
+            'train_loss': average_train_loss,
+            'val_loss': average_val_loss,
+            'best_loss': best_val_loss
+        }
+        save_checkpoint(checkpoint_state, filename=os.path.join(checkpoints_path, 'checkpoint_latest.pth.tar'))
+        if average_val_loss < best_val_loss:
+            best_val_loss = average_val_loss
+            save_checkpoint(checkpoint_state, filename=os.path.join(checkpoints_path, 'checkpoint_best.pth.tar'))
+        save_checkpoint(checkpoint_state, filename=os.path.join(checkpoints_path, f'checkpoint_epoch_{epoch+1}.pth.tar'))
 
     save_last_latent_vectors_to_hdf5(data_path, dict_latent_codes, latent_codes)  # Save the final state of latent codes
-    save_timing_data('timing_data.csv', epoch+1, total_train_time, total_validation_time, total_weight_optimization_time, total_latent_optimization_time)
     np.save(os.path.join(checkpoints_path, 'loss_history.npy'), loss_history)
-    save_timing_data(os.path.join(plots_path, f'timing_data.csv'', timing_data)
+    save_timing_data(os.path.join(plots_path, f'timing_data.csv'), timing_data)
 
 
 def plot_losses(loss_history_path,plots_path):
@@ -316,7 +381,7 @@ def plot_losses(loss_history_path,plots_path):
     plt.title('Training and Validation Losses Over Epochs')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
-    # plt.ylim(0,1)
+    plt.ylim(0,10)
     plt.legend()
     plt.grid(True)
     plt.savefig(os.path.join(plots_path, f'lossesandbeforeopt.png'))
@@ -347,7 +412,7 @@ def main():
     start_epoch, scheduler_g, scheduler_l, best_val_loss = load_checkpoints(generator, latent_vectors, optimizer_g, optimizer_l, checkpoints_path, train_loader, config, device)
 
     print("Starting training and validation...")
-    train_and_validate(generator, latent_vectors, dict_latent_codes, optimizer_g, optimizer_l, scheduler_g, scheduler_l, train_loader, val_loader, start_epoch, num_epochs, checkpoints_path, latent_path, device, best_val_loss, data_path)
+    train_and_validate(generator, latent_vectors, dict_latent_codes, optimizer_g, optimizer_l, scheduler_g, scheduler_l, train_loader, val_loader, start_epoch, num_epochs, checkpoints_path, latent_path, device, best_val_loss, data_path, plots_path)
 
     print("Plotting losses...")
     plot_losses(os.path.join(checkpoints_path, 'loss_history.npy'), plots_path)
